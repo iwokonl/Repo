616843efc4ffec7a19ad5d8115e56d4531b441bd
iwokonl <iwokonl@wp.pl>
2022-03-02 11:34:32 +0100

    Rename main.py to zajecia1.py

--- zajecia2.py
+++ zajecia2.py
@@ -16,11 +16,12 @@ b = 7
 # print(a)
 # print(type(a))
 
-a = int(input('podaj a liczbe:'))
-b = int(input('podaj b liczbe:'))
-c = int(input('podaj c liczbe:'))
-d = int(input('podaj d liczbe:'))
-if (a>b) & (c>d):
-    print("a jest większe od b i c jest większe od d")
-else:
-    print("podany warunek nie zachodzi")
+# a = int(input('podaj a liczbe:'))
+# b = int(input('podaj b liczbe:'))
+# c = int(input('podaj c liczbe:'))
+# d = int(input('podaj d liczbe:'))
+# if (a>b) & (c>d):
+#     print("a jest większe od b i c jest większe od d")
+# else:
+#     print("podany warunek nie zachodzi")
+
