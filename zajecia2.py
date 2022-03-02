da6910d580a6c2e2256308c0c56d7684790572f4
iwokonl <37576228+iwokonl@users.noreply.github.com>
2022-03-02 11:03:09 +0100

    Rename main.py to zajecia1.py

---
+++ zajecia2.py
@@ -0,0 +1,17 @@
+a = 3
+b = 7
+# if a > b:
+#     print('liczba a jest większa od b')
+# elif b > a:
+#     print('liczba a jest większa od b')
+# else:
+#     print('są sobie równe')
+# if a==b:
+#     print('A jest równe B')
+# else:
+#     print('Nie są sobie równe')
+
+a =input('podaj liczbe:')
+a=int(a)
+print(a)
+print(type(a))
