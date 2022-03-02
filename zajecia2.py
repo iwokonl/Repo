73e40e9da79199289df8dbd100d56023f6f20082
iwokonl <iwokonl@wp.pl>
2022-03-02 11:32:03 +0100

    Rename main.py to zajecia1.py

--- zajecia2.py
+++ zajecia2.py
@@ -11,7 +11,16 @@ b = 7
 # else:
 #     print('Nie są sobie równe')
 
-a =input('podaj liczbe:')
-a=int(a)
-print(a)
-print(type(a))
+# a =input('podaj liczbe:')
+# a=int(a)
+# print(a)
+# print(type(a))
+
+a = int(input('podaj a liczbe:'))
+b = int(input('podaj b liczbe:'))
+c = int(input('podaj c liczbe:'))
+d = int(input('podaj d liczbe:'))
+if (a>b) & (c>d):
+    print("a jest większe od b i c jest większe od d")
+else:
+    print("podany warunek nie zachodzi")
