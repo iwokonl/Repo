b2ee229d1d906c57c530846ba5d2e770dc86b5d1
iwokonl <37576228+iwokonl@users.noreply.github.com>
2022-03-16 15:52:00 +0100

    Create Lab_1_Pracadomowa.py

---
+++ Lab_1_Pracadomowa.py
@@ -0,0 +1,51 @@
+#zad_1 lab_1
+a = 2
+int(a)
+print(type(a))
+b = 2
+float(b)
+print(type(b))
+c="beka"
+print(type(c))
+d=2j
+print(type(d))
+
+#zad_2 lab_1
+import math
+a=2
+b=4
+print(a+b)
+print(a-b)
+print(a**b)
+print(a/b)
+print(math.sqrt(b))
+
+#zad_4 lab_1
+import math
+import cmath
+print(exp(10))
+
+#zad_5 lab_1
+a="IWO"
+b="STANISLAWSKI"
+x = a.capitalize()
+y = b.capitalize()
+print(x+" "+y)
+
+#zad_6 lab_1
+s="la la la sss"
+print(s.count("la"))
+
+#zad_7 lab_1
+imie = ["i","w","o"]
+print(imie[1] + " " + imie[2])
+
+#zad_8 lab_1
+imie="Iwo Stanisławski beka"
+print(imie.split())
+
+#zad_9 lab_1
+a="Iwo"
+b=1.5
+c=hex(266)
+print(c)
