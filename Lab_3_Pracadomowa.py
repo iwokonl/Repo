d5f985c6f80c69634a7920cd1195869541909a69
iwokonl <37576228+iwokonl@users.noreply.github.com>
2022-03-18 16:42:31 +0100

    Update Lab_3_Pracadomowa.py

--- Lab_3_Pracadomowa.py
+++ Lab_3_Pracadomowa.py
@@ -1,1 +1,44 @@
-
+#Zad1
+import math
+# a = [1-x for x in range(1,11)]
+# print(a)
+# b = [pow(4, x) for x in range(0,8)]
+# print(b)
+# c = [x for x in b if x % 2==0]
+# print(c)
+#Zad2
+# import random
+# liczby = [random.randrange(100) for x in range(10)]
+# lista = [x for x in liczby if x%2==0]
+# print(lista)
+#Zad3
+# slownik = {"chleb":"sztuka", "ziemniaki":"kilogram", "mleko":"litr"}
+# print(slownik)
+# slownik2 = {value: key for key, value in slownik.items()}
+# print(slownik2)
+#Zad4
+# a = input()
+# b = input()
+# c = input()
+# if ((a+b>c) and (a+c>b) and (b+c>a)):
+#     if ((a*a+b*b==c*c) or (a*a+c*c==b*b) or (b*b+c*c==b*b)):
+#         print("Wtedy trójkąt jest prostokątny")
+#     else:
+#         print("Wtedy trójkąt nie jest prostokątny")
+# else:
+#     print("Nie da się zrobić takiego trójkąta")
+#Zad5
+# a = input()
+# b = input()
+# h = input()
+# pole = ((int(a)+int(b))*int(h)/2)
+# print(pole)
+#Zad6
+import math
+# def funkcja (a, b, ile):
+#     ciąg = []
+#Zad 10
+# import random
+# liczby = [random.randrange(100) for x in range(0,100)]
+# lista = [x for x in liczby if x%4==0]
+# print(lista)
