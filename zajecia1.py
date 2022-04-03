97b56fc867d8fa448091603afb5c2481d136ff54
iwokonl <37576228+iwokonl@users.noreply.github.com>
2022-04-03 15:24:40 +0200

    Update zajecia1.py

--- zajecia1.py
+++ zajecia1.py
@@ -1,39 +1,39 @@
-# import datetime
-# import math
-# import sys
-#
-# a="Beczka"
-# print(sys.version)
-#
-# print(a)
-# b=5
-#
-# print(type(b))
-# c=5.9
-#
-# print(type(c))
-# d=3+2j
-# print(type(d))
-#
-# nazwa_zmiennej = 4
-# del nazwa_zmiennej
-# del a
-# # Komentarze
-#
-# # crtl
-# # \
-#
-# a,f = 3,"iwo"
-# print(a,f)
-#
-# g=2
-# print(a//g)
-# print(a**g)
-# print((1/2)**g)
-# print(math.pow(1/2,a))
+import datetime
+import math
+import sys
 
-# a=5
-# b=3
-# c=5*3
-# print("Wynik dzielenia %(z1)d - %(z2)d = %(z3)d" %{'z1':a, 'z2':b, 'z3':c})
-#d
+a="Beczka"
+print(sys.version)
+
+print(a)
+b=5
+
+print(type(b))
+c=5.9
+
+print(type(c))
+d=3+2j
+print(type(d))
+
+nazwa_zmiennej = 4
+del nazwa_zmiennej
+del a
+# Komentarze
+
+# crtl
+# \
+
+a,f = 3,"iwo"
+print(a,f)
+
+g=2
+print(a//g)
+print(a**g)
+print((1/2)**g)
+print(math.pow(1/2,a))
+
+a=5
+b=3
+c=5*3
+print("Wynik dzielenia %(z1)d - %(z2)d = %(z3)d" %{'z1':a, 'z2':b, 'z3':c})
+d
