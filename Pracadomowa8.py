ace491bc991ec1336068544e46b3bd0c68f3c48f
iwokonl <37576228+iwokonl@users.noreply.github.com>
2022-05-27 16:01:23 +0200

    Update Pracadomowa8.py

--- Pracadomowa8.py
+++ Pracadomowa8.py
@@ -1,1 +1,26 @@
+import numpy as np
+# # Zadanie 1
+# zad1 = np.arange(4, 81, 4)
+# print(zad1)
 
+# # Zadanie 2
+# zad2 = np.arange(5, dtype = 'float64')
+# print(zad2)
+# print(zad2.dtype)
+# zad2_k = zad2.astype('int')
+# print(zad2_k)
+# print(zad2_k.dtype)
+
+# # Zadanie 3
+# def zadanie3 (n=3):
+#     tab = np.array([2**a for a in range(n*n)])
+#     return tab.reshape((n,n))
+# n = input('Podaj n: ')
+# n = int(n)
+# print(zadanie3(n))
+
+# # Zadanie 4
+# def zadanie4 (liczba, ilosc):
+#     return np.logspace (1, ilosc, num=ilosc, base=liczba)
+#
+# print(zadanie4(2, 3))
